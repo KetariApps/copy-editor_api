0b8cb3eef5702973cc07a84cadd5d3ff68d3fd4e
Zach Garrett <zach@ketari.io>
2023-06-02 17:20:30 -0500

    update get

--- src/index.ts
+++ src/index.ts
@@ -22,19 +22,25 @@ const workers: Map<string, Worker> = new Map();
 app.get("/sse", (req: Request, res: Response) => {
   const streamId = req.query.streamId as string;
   const worker = workers.get(streamId);
+
   if (worker === undefined) {
-    res.write("Error locating worker", () => res.end());
+    res.status(404).send("Error locating worker");
+    return;
   }
 
-  worker?.on("message", (message: string) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  worker.on("message", (message: string) => {
     if (message === "done") {
       workers.delete(streamId);
       res.end();
-      return;
     } else if (message === "error") {
-      console.error("error from worker");
+      console.error("Error from worker");
     } else {
-      res.write(message);
+      res.write(`data: ${message}\n\n`);
     }
   });
 });
@@ -47,9 +53,8 @@ app.post("/edit", (req: Request, res: Response) => {
   res.setHeader("Connection", "keep-alive");
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  const content = req.body.message; // Assuming the request contains a "message" property
+  const content = req.body.message;
   const streamId = uuid();
-  // const streamId = uuid();
   const workerData: RequestEditsWorkerData = {
     streamId,
     content,
@@ -59,10 +64,8 @@ app.post("/edit", (req: Request, res: Response) => {
   });
   workers.set(streamId, worker);
   res.write(JSON.stringify({ streamId }), () => {
-    // console.log("sent response, closing stream");
     res.end();
   });
-  // return await requestEdits(userMessage, openai, emitter).then((_) => userId);
 });
 
 // Start the server
