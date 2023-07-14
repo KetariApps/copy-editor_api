e08584766d5a34f19b41ce75f92b2c324c0ce1de
zachagarrett <zach.garrett.archery@gmail.com>
2023-07-14 12:29:49 -0500

    update testReq

--- testReq.sh
+++ testReq.sh
@@ -9,8 +9,6 @@ message="{\"message\":\"$message_text\"}"
 # Send text to the edits endpoint and retrieve SSE key
 stream_id=$(curl -X POST -H "Content-Type: application/json" -d "$message" http://localhost:4000/edit | sed -n 's/.*"streamId":"\([^"]*\)".*/\1/p')
 
-# Replace {PORT} with the actual port number you want to use
-
 # Make request to SSE endpoint using the obtained SSE key
 curl -N "http://localhost:4000/sse?streamId=${stream_id}"
 
